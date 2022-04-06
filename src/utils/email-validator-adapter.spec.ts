2f90dc0d8f6848fe0f52b8382ef8414812060ef6
Caua Henrique <caua.henrique@squadra.com.br>
2022-04-06 15:31:47 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -17,7 +17,14 @@ describe('EmailValidator Adapter', () => {
 
     test('Should return false if validator returns true', () => {
         const sut = new EmailValidatorAdapter()
-        const isValid = sut.isValid('invalid_email@mail.com')
+        const isValid = sut.isValid('valid_email@mail.com')
         expect(isValid).toBe(true)
+    })
+
+    test('Should call validator with correct email', () => {
+        const sut = new EmailValidatorAdapter()
+        const validatorSpy = jest.spyOn(validator, 'isEmail')
+        sut.isValid('any_email@mail.com')
+        expect(validatorSpy).toHaveBeenCalledWith('any_email@mail.com')
     })
 })
