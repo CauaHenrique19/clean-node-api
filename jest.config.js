44b63d4f02baf0ee5e42e5aa404280a6c0307f83
Caua Henrique <caua.henrique@squadra.com.br>
2022-04-06 08:57:50 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: [
+    '<rootDir>/src'
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "node",
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+};
