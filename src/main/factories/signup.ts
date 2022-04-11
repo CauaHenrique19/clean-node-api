373aede1a213d7d88759d172a3bc2dba9d0a42d2
Caua Henrique <caua.henrique@squadra.com.br>
2022-04-11 09:46:12 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -4,7 +4,6 @@ import { AccountMongoRepository } from '../../infra/db/mongodb/account-repositor
 import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
-import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { LogControllerDecorator } from '../decorators/log'
 import { makeSignUpValidation } from './signup-validation'
 
@@ -13,8 +12,7 @@ export const makeSignUpController = (): Controller => {
     const addAccountMongoRepository = new AccountMongoRepository()
     const encrypter = new BcrypAdapter(SALT)
     const addAccount = new DbAddAccount(encrypter, addAccountMongoRepository)
-    const emailValidator = new EmailValidatorAdapter()
-    const signUpController = new SignUpController(emailValidator, addAccount, makeSignUpValidation())
+    const signUpController = new SignUpController(addAccount, makeSignUpValidation())
     const logMongoRepository = new LogMongoRepository()
     return new LogControllerDecorator(signUpController, logMongoRepository)
 }
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,10 +1,8 @@
-import { InvalidParamError } from "../../errors"
 import { badRequest, ok, serverError } from "../../helpers/http-helper"
-import { Controller, EmailValidator, HttpRequest, HttpResponse, AddAccount, Validation } from "./signup-protocols"
+import { Controller, HttpRequest, HttpResponse, AddAccount, Validation } from "./signup-protocols"
 
 export class SignUpController implements Controller {
     constructor(
-        private readonly emailValidator: EmailValidator,
         private readonly addAccount: AddAccount,
         private readonly validation: Validation
     ) { }
@@ -12,17 +10,9 @@ export class SignUpController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
             const error = this.validation.validate(httpRequest.body)
-
             if (error) return badRequest(error)
 
-            const { name, email, password, passwordConfirmation } = httpRequest.body
-
-
-            const isValid = this.emailValidator.isValid(email)
-            if (!isValid) {
-                return badRequest(new InvalidParamError('email'))
-            }
-
+            const { name, email, password } = httpRequest.body
             const account = await this.addAccount.add({ name, email, password })
 
             return ok(account)
