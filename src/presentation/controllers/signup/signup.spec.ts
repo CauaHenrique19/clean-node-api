0ce6d522ed9012789c36b64918e318dec3fd1334
Caua Henrique <caua.henrique@squadra.com.br>
2022-04-11 09:11:44 -0300

    refactor: move password comparation to the ValidationComposite

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -71,21 +71,6 @@ const makeSut = (): SutTypes => {
 }
 
 describe('SignUp Controller', () => {
-    test('Should Return 400 if password confirmation fails', async () => {
-        const { sut } = makeSut()
-        const httpRequest = {
-            body: {
-                email: 'any_email@mail.com',
-                name: 'any_name',
-                password: '123',
-                passwordConfirmation: 'invalid_password'
-            }
-        }
-
-        const httpResponse = await sut.handle(httpRequest)
-        expect(httpResponse).toEqual(badRequest(new InvalidParamError('passwordConfirmation')))
-    })
-
     test('Should Return 400 if an invalid email is provided', async () => {
         const { sut, emailValidatorStub } = makeSut()
         jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
