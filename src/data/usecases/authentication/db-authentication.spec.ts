e36f4a4f47b07df305c41b6a67884210a9c552a7
Caua Henrique <caua.henrique@squadra.com.br>
2022-06-27 14:40:58 -0300

    test: ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -46,12 +46,20 @@ describe('DbAuthentication UseCase', () => {
 
         expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
     })
-
+    
     test('Should throw if LoadAccountByEmail throws', async () => {
         const { sut, loadAccountByEmailRepositoryStub } = makeSut()
         jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
         const promise = sut.auth(makeFakeAuthentication())
-
+        
         await expect(promise).rejects.toThrow()
+    })
+
+    test('Should return null if LoadAccountByEmail returns null', async () => {
+        const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+        jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+        const accessToken = await sut.auth(makeFakeAuthentication())
+
+        expect(accessToken).toBeNull()
     })
 })
