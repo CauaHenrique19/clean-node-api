077c89c268d8981096376c169682901621f2e3d7
Caua Henrique <caua.henrique@squadra.com.br>
2022-06-29 10:23:17 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -22,4 +22,12 @@ describe('Jwt Adapter', () => {
 
         expect(accesToken).toBe('any_token')
     })
+
+    test('Should throw if sign throws', async () => {
+        const sut = new JwtAdapter('secret')
+        jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+        const promise = sut.encrypt('any_id')
+
+        await expect(promise).rejects.toThrow()
+    })
 })
