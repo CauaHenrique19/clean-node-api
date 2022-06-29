e72ae355671d4439bdc5f0de3e6cb60e2468b1b2
Caua Henrique <caua.henrique@squadra.com.br>
2022-06-29 11:07:28 -0300

    test: ensure AccountMongoRepository updates the account AccessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -59,4 +59,21 @@ describe('Account Mongo Repository', () => {
 
         expect(account).toBeFalsy()
     })
+
+    test('Should update the account accessToken on updateAccessToken success', async () => {
+        const sut = makeSut()
+        const res = await accountCollection.insertOne({ 
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            password: 'any_password' 
+        })
+
+        const fakeAccount = res
+        
+        await sut.updateAccessToken(fakeAccount.insertedId.toHexString(), 'any_token')
+        const account = await accountCollection.findOne({ _id: fakeAccount.insertedId })
+
+        expect(account).toBeTruthy()
+        expect(account.accessToken).toBe('any_token')
+    })
 })
