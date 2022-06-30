7cfee951d94797fd746ef625736b67bb98adbfc8
Caua Henrique <caua.henrique@squadra.com.br>
2022-06-30 15:10:08 -0300

    test: ensure AuthMiddleware returns 403 if LoadAccountByToken returns null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -42,7 +42,7 @@ const makeSut = () : SutTypes => {
 }
 
 describe('Auth Middleware', () => {
-    test('Should return 403 if no x-access-token exists no headers', async () => {
+    test('Should return 403 if no x-access-token exists in headers', async () => {
         const { sut } = makeSut()
 
         const httpResponse = await sut.handle({})
@@ -56,5 +56,14 @@ describe('Auth Middleware', () => {
         await sut.handle(makeFakeRequest())
 
         expect(loadSpy).toHaveBeenCalledWith('any_token')
+    })
+
+    test('Should return 403 if LoadAccountByToken returns null', async () => {
+        const { sut, loadAccountByTokenStub } = makeSut()
+
+        jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+
+        const httpResponse = await sut.handle({})
+        expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
     })
 })
